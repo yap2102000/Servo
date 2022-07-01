133afb6fbc0fe334bbda2d2979e1a3be34d93c5a
yap2102000 <59398255+yap2102000@users.noreply.github.com>
2022-07-02 02:36:36 +0530

    Update Servo.cpp
    due to some 360 degree servo support issue . after that it probably work smoothly for 360 degree servo motors also.

--- src/avr/Servo.cpp
+++ src/avr/Servo.cpp
@@ -266,8 +266,8 @@ void Servo::write(int value)
   if(value < MIN_PULSE_WIDTH)
   {  // treat values less than 544 as angles in degrees (valid values in microseconds are handled as microseconds)
     if(value < 0) value = 0;
-    if(value > 180) value = 180;
-    value = map(value, 0, 180, SERVO_MIN(),  SERVO_MAX());
+    if(value > 360) value = 360;
+    value = map(value, 0, 360, SERVO_MIN(),  SERVO_MAX());
   }
   this->writeMicroseconds(value);
 }
@@ -295,7 +295,7 @@ void Servo::writeMicroseconds(int value)
 
 int Servo::read() // return the value as degrees
 {
-  return  map( this->readMicroseconds()+1, SERVO_MIN(), SERVO_MAX(), 0, 180);
+  return  map( this->readMicroseconds()+1, SERVO_MIN(), SERVO_MAX(), 0, 360);
 }
 
 int Servo::readMicroseconds()
